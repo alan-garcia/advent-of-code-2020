be118418ea12ceb83e16fbbc649b5c53e3a9483f
alan-garcia <alangarcianavarro@hotmail.com>
2020-12-07 16:46:03 +0100

    Day 7

---
+++ AdventOfCode2020/Day7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = Environment.CurrentDirectory + @"\input.dat";
+            if (File.Exists(path))
+            {
+                List<string> allBags = File.ReadAllLines(path).ToList();
+                var allBagsWithQuantities = Star1.GetBagsWithGroupQuantities(allBags);
+
+                bool BagContains(string color) => allBagsWithQuantities[color].ContainsKey("shiny gold") || allBagsWithQuantities[color].Keys.Any(BagContains);
+                int relatedBagsContainsShinyGold = allBagsWithQuantities.Keys.Count(BagContains);
+
+                Console.WriteLine(" --- STAR 1 ---");
+                Console.WriteLine($"Number of related bags that contains 'shiny gold' bag: { relatedBagsContainsShinyGold }");
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventOfCode2020/Day7/Star1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day7
+{
+    public class Star1
+    {
+        public static Dictionary<string, Dictionary<string, int>> GetBagsWithGroupQuantities(List<string> allBags)
+        {
+            Regex regularExpr = new Regex(@"(.*?) bags contain(?: (\d+ .*?) bag(?:s)?[,.])*", RegexOptions.Compiled);
+
+            var allBagsWithQuantities = allBags.Select(x => regularExpr.Match(x))
+                .ToDictionary(
+                    regMatch => regMatch.Groups[1].Value,
+                    regMatch => regMatch.Groups[2].Captures.OfType<Capture>()
+                        .Select(capture => capture.Value.Split(new[] { ' ' }, 2))
+                        .ToDictionary(quantity => quantity[1], quantity => int.Parse(quantity[0]))
+                );
+
+            return allBagsWithQuantities;
+        }
+    }
+}
