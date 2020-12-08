338cc61ef913410e9aad953b156b2a465c1c0e00
alan-garcia <alangarcianavarro@hotmail.com>
2020-12-08 17:19:50 +0100

    Day 8

---
+++ AdventOfCode2020/Day8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = Environment.CurrentDirectory + @"\input.dat";
+            if (File.Exists(path))
+            {
+                List<string> instructions = File.ReadAllLines(path).ToList();
+                int totalAccumulator = Star1.GetTotalAccumulator(instructions);
+
+                Console.WriteLine(" --- STAR 1 ---");
+                Console.WriteLine($"Total accumulator: { totalAccumulator }");
+                Console.WriteLine();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventOfCode2020/Day8/Star1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Day8
+{
+    public class Star1
+    {
+        public static int GetTotalAccumulator(List<string> instructions)
+        {
+            int accumulator = 0;
+            List<int> seen = new List<int>();
+
+            for (int i = 0; !seen.Contains(i); i++)
+            {
+                seen.Add(i);
+                string operation = instructions[i].Split()[0];
+                int argument = int.Parse(instructions[i].Split()[1]);
+
+                if (operation == "acc")
+                {
+                    accumulator += argument;
+                }
+                else if (operation == "jmp")
+                {
+                    i += argument - 1;
+                }
+            }
+
+            return accumulator;
+        }
+    }
+}
